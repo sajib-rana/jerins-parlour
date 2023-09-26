661d7555db8e3a90991ffff5a160318beefc4f78
sajib-rana <sajibrana444@gmail.com>
2023-09-26 20:04:49 +0600

    jerins parlour project initilize

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Header from "../pages/Shared/Navbar/Navbar";
+import Footer from "../pages/Shared/Footer/Footer";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>this is Footer section</h1>
+        </div>
+    );
+};
+
+export default Footer;
