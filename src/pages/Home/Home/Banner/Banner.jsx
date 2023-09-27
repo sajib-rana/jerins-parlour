50dc49d8237ff5050a64045e0530e0045775907d
sajib-rana <sajibrana444@gmail.com>
2023-09-27 12:01:30 +0600

    services

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -4,19 +4,18 @@ const Banner = () => {
     return (
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src={beautiful}
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
+          <img src={beautiful} className="max-w-sm rounded-lg shadow-2xl" />
           <div>
             <h1 className="text-5xl uppercase font-bold">Beauty Salon </h1>
             <h1 className="text-5xl uppercase font-bold mt-2">
               for every women
             </h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Parlours are commonly used for businesses that offer services. For
+              example, a beauty parlour is where customers go to get haircuts,
+              facials, manicures, and other beauty treatments. In marketing,
+              this physical space serves as the primary point of interaction
+              between the business and its customers.
             </p>
             <button className="btn bg-rose-600">Get an Appointment</button>
           </div>
---
+++ src/pages/Home/Home/Services/Services.jsx
@@ -0,0 +1,48 @@
+import img1 from '../../../../assets/icons/Group 1372.png'
+import img2 from '../../../../assets/icons/Group 1373.png'
+import img3 from '../../../../assets/icons/Group 1374.png'
+import "animate.css";
+
+const Services = () => {
+    return (
+      <div className="mt-10">
+        <h1 className="text-center text-2xl font-bold">
+          Our Awesome <span className="text-rose-600">Services</span>
+        </h1>
+        <div className="flex justify-center gap-10 mt-5 mb-10  ">
+          <div className="card w-96 bg-base-100 shadow-xl animate__animated animate__zoomIn ">
+            <figure className="px-10 pt-10">
+              <img src={img1} alt="Shoes" className="rounded-xl w-12" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h1 className="text-xl font-bold">Hair color & Styleing</h1>
+              <h2 className="card-title">$99</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl animate__animated animate__zoomIn">
+            <figure className="px-10 pt-10">
+              <img src={img2} alt="Shoes" className="rounded-xl w-12" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h1 className="text-xl font-bold">Skin care & Treatment</h1>
+              <h2 className="card-title">$299</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl animate__animated animate__zoomIn">
+            <figure className="px-10 pt-10">
+              <img src={img3} alt="Shoes" className="rounded-xl w-12" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h1 className="text-xl font-bold">Anti age face Treatment</h1>
+              <h2 className="card-title">$199</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Services;
